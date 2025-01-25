2caf73618aaa27222bd7266b8262ef08252331da
El Pablo <42917478+p-kravtsov@users.noreply.github.com>
2025-01-25 14:54:56 +0100

    Add game clock, background color and FPS limit

--- an_adventure.py
+++ an_adventure.py
@@ -9,8 +9,10 @@ class AnAdventure:
     def __init__(self):
         """| Initialize the game, and create game resources |"""
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))  # Create a display window
         pygame.display.set_caption("An Adventure")
+        self.bg_color = (234, 230, 230)
 
     def run_game(self):
         """| Start the main loop for the game |"""
@@ -21,8 +23,12 @@ class AnAdventure:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the
+            self.screen.fill(self.bg_color)
+
             # Make only a portion of the screen to be updated, instead of the entire area
             pygame.display.update()  # pygame.display.flip() | 229 | https://www.pygame.org/docs/ref/display.html#pygame.display.update |
+            self.clock.tick(60)  # Limit the while loop to run at 60 FPS
 
 
 if __name__ == "__main__":
