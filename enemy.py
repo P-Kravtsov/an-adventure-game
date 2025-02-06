79683d99f70be8fc38cd0b297fda0cfc2a080e0c
El Pablo <42917478+p-kravtsov@users.noreply.github.com>
2025-02-06 21:31:35 +0100

    refactor: put img loading to `_load_image` method
    Encapsulate the Image Loading Logic &
    Improve code readability

--- enemy.py
+++ enemy.py
@@ -9,13 +9,6 @@ class Enemy:
         self.screen = game.screen
         self.settings = game.settings
 
-        # Load the specified enemy image (default: 'enemy.bmp').
-        try:
-            self.image = pygame.image.load(image_path)
-        except pygame.error as e:
-            print(f"Error loading enemy image at {image_path}: {e}")
-            raise
-
         # Set its rectangular position and size.
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -24,6 +17,16 @@ class Enemy:
         # Store the position as floats for fine-tuned movement.
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
+
+        self.image = self._load_image(image_path)
+
+    def _load_image(self, image_path):
+        """| Load the specified enemy image (default: 'enemy.bmp') |"""
+        try:
+            return pygame.image.load(image_path)
+        except pygame.error as e:
+            print(f"Error loading enemy image at {image_path}: {e}")
+            raise
 
     def update(self):
         """Update the enemy's position or behavior (if any)."""
